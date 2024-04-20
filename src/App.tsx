2b8f7decc4b0f571a121f7146a8703f6cebc8101
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-04-20 23:33:42 +0900

    build: vite.config 파일에 메인 scss 추가, @ 사용가능, scssEx페이지 추가, 컴포넌트 폴더 gitkeep 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import "./style.scss";
+import ScssExample from "./components/scss-example";
+import { useState } from "react";
+
 function App() {
+  const [currentPage, setCurrentPage] = useState("");
+
+  const handleRouteEx = () => {
+    setCurrentPage("");
+  };
+
+  if (currentPage === "ex") {
+    return <ScssExample onRouteEx={handleRouteEx} />;
+  }
+
   return (
-    <main>
-      <article>윤수쓰 파트</article>
-      <article>지수쓰 파트</article>
-      <article>지원쓰 파트</article>
-    </main>
+    <>
+      <main>
+        <article>윤수쓰 파트</article>
+        <article>지수쓰 파트</article>
+        <article>
+          <button
+            style={{ backgroundColor: "red", padding: "24px" }}
+            onClick={() => setCurrentPage("ex")}
+          >
+            scss 예시 페이지로 이동
+          </button>
+          지원쓰 파트
+        </article>
+      </main>
+    </>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": resolve(__dirname, `./src`),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/main.scss" as *;`,
+      },
+    },
+  },
+});
