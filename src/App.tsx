5317ec860a095bdc4ab29a9fdf91d7a69d327e3c
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-04-20 21:18:44 +0900

    feat: 프로젝트 시작

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <article>윤수쓰 파트</article>
+      <article>지수쓰 파트</article>
+      <article>지원쓰 파트</article>
+    </main>
+  );
+}
+
+export default App;
